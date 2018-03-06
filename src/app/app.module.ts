3447be005e7c996f2c181da89ee47c006e8f1039
Scrip <ygenthon.pierre@gmail.com>
2018-03-06 15:15:51 +0100

    poc chart integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {LineChart} from "./LineChart.component";
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChart
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
