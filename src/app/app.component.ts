606e0eb1d59cb7e44dbb2dc850570589b5eacdc6
ZEAMARI MOHAMED <moh.67.100@gmail.com>
2018-03-07 15:08:23 +0100

    verify the connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hive } from './Models/Hive';
-import {BeeDataService} from './services/bee-data.service';
-import {Observable} from 'rxjs/Rx';
+import { BeeDataService } from './services/bee-data.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +11,23 @@ import {Observable} from 'rxjs/Rx';
 export class AppComponent implements OnInit {
 
   public myHive: Hive;
+  public connection : boolean;
 
   constructor(private beedataService: BeeDataService) {
-  
+
   }
 
   ngOnInit() {
-    let timer = Observable.timer(2000,5000);
-    timer.subscribe(t=> {
+    let timer = Observable.timer(2000, 5000);
+    timer.subscribe(t => {
+      this.connection = navigator.onLine;
+      if (navigator.onLine) {
         this.getDatas();
+      }
     });
   }
 
- 
-
-  public getDatas(){
+  public getDatas() {
     this.beedataService.getData().subscribe(da => {
       console.log(da);
       this.myHive = new Hive(da.temperature, da.hatchOpen, da.vibration, da.soundActivity, da.dateTime);
