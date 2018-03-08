b2d4d1331f02af4ed47a7035232b5d4c3ed13ba1
HorvatAntoine <horvatantoine@gmail.com>
2018-03-08 11:09:46 +0100

    Ajout du fichier de config ngsw et des références du sw dans app.module.ts

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,3 +9,30 @@ self.addEventListener('install', function(evt) {
     evt.respondWith(fromCache(evt.request));
     evt.waitUntil(update(evt.request));
 });
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      './controlled.html',
+      './asset'
+    ]);
+  });
+}
+
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+    }, reject);
+  });
+}
